133c42d85ae4c50adfcf8e6b803fb27c2f10fd42
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2022-03-26 20:42:00 +0100

    add dev-support scripts for DB resetting

--- backend/de.metas.cucumber/dev-support/20_convert_db_to_template.sh
+++ backend/de.metas.cucumber/dev-support/20_convert_db_to_template.sh
@@ -9,4 +9,9 @@ winpty docker exec -it infrastructure_db_1  psql -U postgres -c "alter database 
 winpty docker exec -it infrastructure_db_1  psql -U postgres -c "alter database metasfresh_template_master_integration is_template true;"
 
 echo "The local database has been converted to a template database."
+echo "You can drop this template database by running the following commands:"
+echo ""
+echo "winpty docker exec -it infrastructure_db_1  psql -U postgres -c \"UPDATE pg_database SET datistemplate='false' WHERE datname='metasfresh_template_master_integration';\""
+echo "winpty docker exec -it infrastructure_db_1  psql -U postgres -c \"drop database if exists metasfresh_template_master_integration;\""
+echo ""
 echo "You can now proceed with creating the actual database from this template"
--- backend/de.metas.cucumber/dev-support/30_reset_db_to_template.sh
+++ backend/de.metas.cucumber/dev-support/30_reset_db_to_template.sh
@@ -9,4 +9,4 @@ winpty docker exec -it infrastructure_db_1  psql -U postgres -c "drop database i
 winpty docker exec -it infrastructure_db_1  psql -U postgres -c "create database metasfresh template metasfresh_template_master_integration;"
 
 echo "The local database has been recreated from the template database."
-echo "You can rerun this script to reset the local database with this script"
+echo "You can rerun this script to reset the local database to the template database."
